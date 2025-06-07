32040dc4489b15f3db21fe5242497e58bcb6ac2e
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-06-07 14:31:40 -0400

    Feat: Setup y testeo del TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
     return (
-        <>
-            <h1>Hola Mundo</h1>
-        </>
+        <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center'>
+            <h1 className='text-4xl font-bold text-blue-700'>
+                ¡Hola Mundo, pero con Tailwind CSS v4 en Vite!
+            </h1>
+        </div>
     );
 }
 
